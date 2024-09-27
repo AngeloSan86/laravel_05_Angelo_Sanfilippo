6864b4e823a052236fdc25175ab52f8ed26e13c2
Angelo Sanfilippo <angelosanfilippoct@gmail.com>
2024-09-27 02:21:53 +0200

    invio mail

---
+++ resources/views/mail/contact-mail.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>Ciao {{$username}} </h2>
+<h3>Il tuo messaggio è stato inviato!</h3>
+<ul>
+    
+    <li>
+        <p>La tua mail: {{$useremail}} </p>
+    </li>
+
+    <li>
+        <p>Il tuo messaggio: {{$usermessage}} </p>
+    </li>
+
+</ul>
+
+    
+</body>
+</html>
---
+++ resources/views/thankYou.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <div class="container-fluid">
+        <div class="row height-custom align-items-center justify-content-center">
+            <div class="col-12">
+                <h1 class="text-center display-4 title">
+                    Grazie per averci contattato!
+                </h1>
+                
+            </div>
+        </div>
+    </div>
+</x-layout> 
